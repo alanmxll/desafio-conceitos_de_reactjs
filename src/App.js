c8493dc43e58616bc7e7a882c2275725a7494b92
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-11-29 17:40:41 -0300

    should be able to remove repository: OK

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
@@ -32,7 +34,9 @@ function App() {
         {repositories.map((repository) => (
           <li key={repository.id}>
             {repository.title}
-            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
           </li>
         ))}
       </ul>
